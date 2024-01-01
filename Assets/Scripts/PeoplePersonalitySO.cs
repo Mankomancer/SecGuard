b1f7bd4f5408ba64081fcfa1a3a77147c564d25a
Einars Koops <mankomancer@gmail.com>
2024-01-01 17:14:54 +0200

    Playing with Scriptable Object, making more readable

--- Assets/Scripts/PeoplePersonalitySO.cs
+++ Assets/Scripts/PeoplePersonalitySO.cs
@@ -13,6 +13,7 @@ public class PeoplePersonalitySO : ScriptableObject
     // I KNOW I SHOULD SEPERATE PEOPLE CATEGORIES IN MORE DETAILS (at least 3), BUT BECAUSE I HAVE TIGHT DEADLINE WILL DO THIS LAZY WAY. Not like anyone else will work on code part here
 
     //------------------This part if meant for people who are in concert to watch it
+    [Header("Clients")]
     [SerializeField] string [] random_text_messages;    //what they could say regardless if they were jammed or not. will need to do some % experimentation
     [SerializeField] string [] text_message_after_jamming_coorect;  //how they will react after jamming - they did something bad
 
@@ -25,12 +26,14 @@ public class PeoplePersonalitySO : ScriptableObject
 
 
     //--------------------This part is for people who are in concert working
+    [Header("Workers")]
     [SerializeField] string [] getting_jammed;//when played jamms the NPC. It always is a mistake
     [SerializeField] string [] warning_player; //sees that someone is doing something bad
     [SerializeField] string [] thank_you; //player took care of all if not most people who broke the rules
 
 
     //---------------------This part is for close friends/relatives
+    [Header("Close people")]
     [SerializeField] string [] misc_text_messages; //just stuff relatives will spawn
     
 }
